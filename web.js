8fc583312132ec35ef085f0ed07e1f227d84101b
robinmilan <robin_milan@hotmail.com>
2013-07-17 16:05:18 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var index = 'index.html';
+  var buffer = new Buffer(fs.readFileSync(index));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
